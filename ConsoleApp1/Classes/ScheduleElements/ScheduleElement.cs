d6a799b50c71969f4836a9b5045a7f69d6269cfe
CORP\MurzinAV <a.v.murzin@chelindbank.ru>
2019-03-11 16:53:40 +0500

    20190311-2

--- ConsoleApp1/Classes/ScheduleElements/ScheduleElement.cs
+++ ConsoleApp1/Classes/ScheduleElements/ScheduleElement.cs
@@ -10,8 +10,30 @@ namespace ConsoleApp1.Classes.ScheduleElements
 	{
 		public static ScheduleElement GetElement(string valueString)
 		{
-			
+			/*
+			private static Regex Pattern => new Regex(@"^(?<min>\d+)-(?<max>\d+)(/(?<step>\d+))?$");
+			var matches = Pattern.Matches(inputString);
+			MinValue = int.Parse(matches[0].Groups["min"].Value),
+			MaxValue = int.Parse(matches[0].Groups["max"].Value),
+			Step = matches[0].Groups["step"].Length > 0 ? int.Parse(matches[0].Groups["step"].Value) : 1
+			*/
 
+			/*
+			 private static Regex Pattern => new Regex(@"^\d+$");
+			 public override bool IsMatch(string inputString) => Pattern.IsMatch(inputString);
+			 Value = int.Parse(inputString)
+			*/
+			
+			/*
+			private const char Delimiter = ',';
+			public override bool IsMatch(string inputString) => inputString.Any(ch => ch == Delimiter);
+			var parts = inputString.Split(Delimiter);
+			return new ListScheduleElementValue
+			{
+				Values = parts.Select(p => p.GetScheduleElement()).ToList()
+			};
+			*/
+			throw new NotImplementedException();
 		}
 
 	}
